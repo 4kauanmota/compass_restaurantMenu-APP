d58c3be10661a9507e4d940aac61b2172efa9ba7
Kauan Soares Mota <4kauanmota@gmail.com>
2023-08-09 17:13:20 -0300

    style: using better names to define constants

--- src/api/server.tsx
+++ src/api/server.tsx
@@ -1,21 +1,21 @@
 import Restaurant from "@/models/Restaurant";
 import axios from "axios";
 
-const URL = "https://8jcox47hg2.execute-api.us-east-2.amazonaws.com/dev";
+const API = "https://8jcox47hg2.execute-api.us-east-2.amazonaws.com/dev";
 
 export async function fetchData() {
-  const response = await axios.get(URL);
+  const response = await axios.get(API);
 
   return response;
 }
 
 export async function fetchRestaurants(): Promise<Array<object>> {
-  const response = await axios.get(URL);
-  const result = response.data.body.restaurants;
+  const response = await axios.get(API);
+  const data = response.data.body.restaurants;
 
   const restaurants: Array<object> = [];
 
-  result.map((restaurant: Restaurant) => {
+  data.map((restaurant: Restaurant) => {
     const restaurantObj = {
       name: restaurant.name,
       coverImageUrl: restaurant.coverImageUrl,
@@ -28,10 +28,10 @@ export async function fetchRestaurants(): Promise<Array<object>> {
 }
 
 export async function fetchRestaurant(id: number): Promise<Restaurant> {
-  const response = await axios.get(URL);
-  const result = await response.data.body.restaurants;
+  const response = await axios.get(API);
+  const data = await response.data.body.restaurants;
 
-  const restaurant: Restaurant = result.filter((restaurant: Restaurant) => restaurant.id === id);
+  const restaurant: Restaurant = data.filter((restaurant: Restaurant) => restaurant.id === id);
 
   return restaurant;
 }
