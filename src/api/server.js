386b492e5fbb2931b73c8d1a35e71c99486e984d
Kauan Soares Mota <4kauanmota@gmail.com>
2023-08-09 16:28:17 -0300

    feat: api restaurant fetching

--- src/api/server.js
+++ src/api/server.js
@@ -3,7 +3,28 @@ import axios from "axios";
 const URL = "https://8jcox47hg2.execute-api.us-east-2.amazonaws.com/dev";
 
 export async function fetchData() {
-  const result = axios.get(URL);
+  const response = await axios.get(URL);
 
-  return result;
+  return response;
+}
+
+export async function fetchRestaurants() {
+  const response = await axios.get(URL);
+  const result = response.data.body.restaurants;
+
+  const restaurants = [];
+
+  result.map((restaurant) => {
+    const restaurantObj = {
+      id: restaurant.id,
+      name: restaurant.name,
+      description: restaurant.description,
+      coverImageUrl: restaurant.coverImageUrl,
+      rating: restaurant.rating,
+    };
+
+    restaurants.push(restaurantObj);
+  });
+
+  return restaurants;
 }
