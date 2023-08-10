995a240fbf4ba3c7542fbefb3921a8ffa01fdd5f
Kauan Soares Mota <4kauanmota@gmail.com>
2023-08-10 16:39:43 -0300

    style: code organization

--- src/screens/restaurant/RestaurantDetails.tsx
+++ src/screens/restaurant/RestaurantDetails.tsx
@@ -6,8 +6,9 @@ import {
   ScrollView,
   ImageBackground,
   TouchableWithoutFeedback,
+  StyleSheet
 } from "react-native";
-import { StyleSheet } from "react-native";
+
 
 export default function RestaurantDetails({ navigation }: { navigation: any }) {
   const handlePress = () => {
@@ -16,76 +17,75 @@ export default function RestaurantDetails({ navigation }: { navigation: any }) {
 
   return (
     <ScrollView style={styles.container}>
-      <View style={styles.page}>
-        <ImageBackground
-          style={styles.Image}
-          resizeMode="cover"
-          source={require("../../img/fotoPerfil.png")}
-        >
-          <View style={styles.vector}>
-            <TouchableWithoutFeedback onPress={handlePress}>
-              <Image source={require("../../img/Vector.png")} />
-            </TouchableWithoutFeedback>
-          </View>
-
-          <View style={styles.infheader}>
-            <Text style={styles.TextHeaderinformation}>
-              Churrascaria Barranco
-            </Text>
-            <View style={styles.star}>
-            <Image  source={require("../../img/star.png")} />
-            <Image  source={require("../../img/star.png")} />
-            <Image  source={require("../../img/star.png")} />
-            <Image  source={require("../../img/star.png")} />
-            <Image  source={require("../../img/star.png")} />
-            </View>
-          </View>
-        </ImageBackground>
-
-        <View style={styles.infrestaurante}>
-          <Text style={styles.Textinfrestaurante}>sobre o restaurante</Text>
-
-          <Text style={styles.textcolor}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-            eum impedit, voluptate dolorum veniam magnam quibusdam quia
-            consequatur perferendis modi molestiae excepturi, error tenetur
-            minima aperiam accusantium similique hic qui.
-          </Text>
+      <ImageBackground
+        style={styles.backgroundImage}
+        resizeMode="cover"
+        source={require("../../img/fotoPerfil.png")}
+      >
+        <View style={styles.goBackButton}>
+          <TouchableWithoutFeedback onPress={handlePress}>
+            <Image source={require("../../img/Vector.png")} />
+          </TouchableWithoutFeedback>
         </View>
 
-        <View style={styles.menu}>
-          <Text style={styles.TextMenu}>Menu</Text>
-
-          <View style={styles.cards}>
-
-            <View style={styles.card}>
-              <View style={styles.teste}>
-                <Image source={require("../../img/restaurant.png")}></Image>
-              </View>
-              <View>
-                <Text style={styles.textCard}>Nome comida</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.teste}>
-                <Image source={require("../../img/restaurant.png")}></Image>
-              </View>
-              <View>
-                <Text style={styles.textCard}>Nome comida</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.teste}>
-                <Image source={require("../../img/restaurant.png")}></Image>
-              </View>
-              <View>
-                <Text style={styles.textCard}>Nome comida</Text>
-              </View>
-            </View>
-
+        <View style={styles.informationsContainer}>
+          <Text style={styles.restaurantName}>
+            Churrascaria Barranco
+          </Text>
+          
+          <View style={styles.stars}>
+            <Image id="star1" source={require("../../img/star.png")} />
+            <Image id="star2" source={require("../../img/star.png")} />
+            <Image id="star3" source={require("../../img/star.png")} />
+            <Image id="star4" source={require("../../img/star.png")} />
+            <Image id="star5" source={require("../../img/star.png")} />
           </View>
+        </View>
+      </ImageBackground>
+
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.title}>Sobre o restaurante</Text>
+
+        <Text style={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+          eum impedit, voluptate dolorum veniam magnam quibusdam quia
+          consequatur perferendis modi molestiae excepturi, error tenetur
+          minima aperiam accusantium similique hic qui.
+        </Text>
+      </View>
+
+      <View style={styles.menuContainer}>
+        <Text style={styles.title}>Menu</Text>
+
+        <View style={styles.menuList}>
+
+          <View style={styles.card}>
+            <View style={styles.teste}>
+              <Image source={require("../../img/restaurant.png")}></Image>
+            </View>
+            <View>
+              <Text style={styles.textCard}>Nome comida</Text>
+            </View>
+          </View>
+
+          <View style={styles.card}>
+            <View style={styles.teste}>
+              <Image source={require("../../img/restaurant.png")}></Image>
+            </View>
+            <View>
+              <Text style={styles.textCard}>Nome comida</Text>
+            </View>
+          </View>
+
+          <View style={styles.card}>
+            <View style={styles.teste}>
+              <Image source={require("../../img/restaurant.png")}></Image>
+            </View>
+            <View>
+              <Text style={styles.textCard}>Nome comida</Text>
+            </View>
+          </View>
+
         </View>
       </View>
     </ScrollView>
@@ -95,16 +95,10 @@ export default function RestaurantDetails({ navigation }: { navigation: any }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-
-  page: {
-    width: "100%",
-    height: "100%",
     backgroundColor: "#2c2c2e",
-    alignItems: "center",
   },
 
-  Image: {
+  backgroundImage: {
     overflow: "hidden",
     width: "100%",
     height: 300,
@@ -112,38 +106,38 @@ const styles = StyleSheet.create({
     borderBottomStartRadius: 20,
   },
 
-  vector: {
+  goBackButton: {
     marginTop: 60,
     paddingLeft: 30,
   },
 
-  TextHeaderinformation: {
+  informationsContainer: {
+    height: "70%",
+    marginLeft: 30,
+    justifyContent: "flex-end",
+  },
+
+  restaurantName: {
     color: "#ffffff",
     fontSize: 25,
     fontWeight: "bold",
     width: "50%",
   },
 
-  infheader: {
-    height: "70%",
-    marginLeft: 30,
-    justifyContent: "flex-end",
-  },
-
-  star: {
+  stars: {
     marginTop: 10,
     gap: 10,
     flexDirection: "row",
   },
 
-  infrestaurante: {
+  descriptionContainer: {
     width: "100%",
     padding: 10,
     margin: 10,
     marginBottom: 50,
   },
 
-  menu: {
+  menuContainer: {
     padding: 10,
     flexDirection: "column",
     width: "100%",
@@ -156,23 +150,21 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
 
-  cards: {
+  menuList: {
     flex: 1,
     width: "100%",
-    // borderColor: 'red',
-    // borderWidth: 1,
     flexDirection: "row",
     flexWrap: "wrap",
     gap: 5,
   },
 
   card: {
-    width: 140,
+    width: 170,
     height: 152,
     overflow: "hidden",
     backgroundColor: "#999999",
     borderRadius: 12,
-    margin: 15,
+    margin: 6,
   },
 
   textCard: {
@@ -182,20 +174,14 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 
-  Textinfrestaurante: {
+  title: {
     color: "#ffffff",
     fontSize: 20,
     fontWeight: "bold",
     marginBottom: 10,
   },
 
-  TextMenu: {
-    color: "#ffffff",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-
-  textcolor: {
+  description: {
     color: "#ffffff",
   },
 });
